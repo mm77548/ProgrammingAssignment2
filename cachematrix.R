0bebcf74e297d1a73ae1420f70478c396e634d71
mm77548 <meaganmarziani@aol.com>
2014-10-20 13:02:39 -0400

    cachematrix submission
    Final version to submit for grading.

--- cachematrix.R
+++ cachematrix.R
@@ -27,7 +27,7 @@ cacheSolve <- function(x, ...) {
   
   inv <- x$getinverse()
   if(!is.null(inv)) {
-    message("getting cached data")
+    message("Getting cached data")
     return(inv)
   }
   data <- x$get()
